d04d83a2260cd14dbeaa8920d95d5e14b1486d95
aleeca <49315610+ryoheinan@users.noreply.github.com>
2021-07-03 00:34:49 +0900

    Create create page

---
+++ src/pages/Create.vue
@@ -0,0 +1,106 @@
+<template>
+  <Header :is-login-btn="state.isLoggedin" />
+  <main class="content-bg">
+    <div class="container mx-auto px-5">
+      <div v-if="state.isLoggedin" class="card rounded-lg p-5">
+        <div class="container mx-auto">
+          <label class="block mb-5">
+            <span>イベント名</span>
+            <input
+              type="text"
+              class="
+                mt-1
+                block
+                w-full
+                rounded-md
+                border-transparent
+                focus:bg-white focus:ring-0
+              "
+              placeholder="例）ゲームやる会"
+            />
+          </label>
+          <label class="block mb-5">
+            <span>Discordサーバー名</span>
+            <input
+              type="text"
+              class="mt-1 block w-full rounded-md border-transparent"
+              placeholder="例）ゲーム同好会"
+            />
+          </label>
+          <label class="block mb-5">
+            <span>Discordサーバーの招待コード</span>
+            <input
+              type="url"
+              class="mt-1 block w-full rounded-md border-transparent"
+              placeholder="例）https://discord.gg/xxxyyyzzz"
+            />
+          </label>
+          <label class="block mb-5">
+            <span>イベント開始日</span>
+            <input
+              type="date"
+              class="mt-1 block w-full rounded-md border-transparent"
+            />
+          </label>
+          <label class="block mb-5">
+            <span>イベント開始時間</span>
+            <input
+              type="time"
+              class="mt-1 block w-full rounded-md border-transparent"
+            />
+          </label>
+        </div>
+      </div>
+      <div v-else class="text-center">
+        <h1 class="text-6xl font-bold mb-10">Sign in</h1>
+        <button
+          type="button"
+          class="py-2 px-4 font-semibold rounded-lg focus:outline-none"
+          @click="signin()"
+        >
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { useAuthStore } from '../stores/auth'
+
+  import Header from '../components/Header.vue'
+  export default defineComponent({
+    name: 'Create',
+    components: {
+      Header,
+    },
+    setup: () => {
+      const { signin, state } = useAuthStore()
+      return {
+        signin,
+        state,
+      }
+    },
+  })
+</script>
+
+<style scoped>
+  .emoji {
+    font-size: 6rem;
+  }
+  input {
+    color: #1a1a1a;
+  }
+  .card {
+    background-color: #5865f2;
+    color: #ffffff;
+  }
+  button {
+    background-color: #ffffff;
+    color: #1a1a1a;
+  }
+  button:active {
+    background-color: #cecece;
+  }
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import index from './pages/Index.vue'
 import detail from './pages/Detail.vue'
+import create from './pages/Create.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,11 @@ export const router = createRouter({
     {
       path: '/detail',
       component: detail,
+    },
+    {
+      name: 'Create',
+      path: '/create',
+      component: create,
     },
   ],
 })
