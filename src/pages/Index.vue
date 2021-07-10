5e3b17e8932745ed438e7a709161bbd2840ef386
aleeca <49315610+ryoheinan@users.noreply.github.com>
2021-07-10 21:35:22 +0900

    Index.vueでのFirestoreの設定

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,72 @@
 <template>
   <Header />
   <main class="content-bg">
-    <Events />
+    <div class="container mx-auto px-5">
+      <div
+        class="grid grid-cols-1 lg:grid-cols-3 gap-x-20 gap-y-16 lg:gap-y-12"
+      >
+        <Card
+          v-for="item in state.data"
+          :key="item.id"
+          color="blurple"
+          :emoji="item.emoji"
+          :title="item.eventName"
+          :server-name="item.serverName"
+        />
+      </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import Events from '../components/Events.vue'
+  import { defineComponent, reactive, onMounted } from 'vue'
+  import firebase from 'firebase/app'
+  import 'firebase/firestore'
   import Header from '../components/Header.vue'
+  import Card from '../components/Card.vue'
 
   export default defineComponent({
     name: 'App',
     components: {
       Header,
-      Events,
+      Card,
+    },
+    setup() {
+      const db = firebase.firestore()
+      const recvData: any[] = []
+
+      const state = reactive({
+        data: null as any,
+      })
+
+      const getData = async () => {
+        db.collection('events')
+          .get()
+          .then((query) => {
+            query.forEach((doc) => {
+              let data = doc.data()
+              recvData.push({
+                id: doc.id,
+                eventName: data.name,
+                serverName: data.serverName,
+                emoji: data.emoji,
+              })
+            })
+            return recvData
+          })
+          .then((data) => {
+            state.data = data
+          })
+          .catch(() => {
+            alert('データの取得に失敗しました')
+          })
+      }
+
+      onMounted(() => {
+        getData()
+      })
+
+      return { state, getData }
     },
   })
 </script>
